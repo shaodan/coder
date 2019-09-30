ee1e2b9f5147df87a16e9dbe62cc11c457dff7ec
Dan Shao <shaodan.cn@gmail.com>
2019-09-30 23:39:44 +0800

    test template

---
+++ leetcode/lib_test.py
@@ -0,0 +1,24 @@
+'''
+Definition of TestCase
+'''
+
+class TestCase(object):
+
+    def __init__():
+        pass
+
+    def test():
+        pass
+
+
+class TestSingleLinkedList(TestCase):
+
+    def test():
+        pass
+
+
+class Test():
+
+    def __init__():
+        pass
+
