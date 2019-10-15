9df8a284c8ddc1ecae2d96331121f27311929f48
Dan Shao <shaodan.cn@gmail.com>
2019-10-15 23:40:51 +0800

    problem 543 tree, recursion

---
+++ leetcode/543.diameter-of-binary-tree.c
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=543 lang=c
+ *
+ * [543] Diameter of Binary Tree
+ *
+ * https://leetcode.com/problems/diameter-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (47.53%)
+ * Total Accepted:    166.4K
+ * Total Submissions: 349.6K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 
+ * Given a binary tree, you need to compute the length of the diameter of the
+ * tree. The diameter of a binary tree is the length of the longest path
+ * between any two nodes in a tree. This path may or may not pass through the
+ * root.
+ * 
+ * 
+ * 
+ * Example:
+ * Given a binary tree 
+ * 
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       2   3
+ * ⁠      / \     
+ * ⁠     4   5    
+ * 
+ * 
+ * 
+ * Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+ * 
+ * 
+ * Note:
+ * The length of path between two nodes is represented by the number of edges
+ * between them.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int max;
+int diameterOfBinaryTree(struct TreeNode* root){
+    if (root == NULL) {
+        return 0;
+    }
+    // important! reset result between testcases
+    max = 0;
+    depthOfTree(root);
+    return max;
+}
+
+int depthOfTree(struct TreeNode* root) {
+    int left = root->left == NULL ? 0 : depthOfTree(root->left);
+    int right = root->right == NULL ? 0 : depthOfTree(root->right);
+    int sum = left + right;
+    if (sum > max) {
+        max = sum;
+    }
+    return (left > right ? left : right ) + 1;
+}
+
